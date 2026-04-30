22e201d2011fbd7c377e9d95d71bbe3b8ec72456
Ab0umar <dr.elsa3dany@gmail.com>
2026-04-30 07:02:12 +0300

    chore: keep only important files by updating .gitignore and index

--- GIT.ps1
+++
@@ -1,7 +0,0 @@
-Write-Output "# Web.selrs" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Ab0umar/Web.selrs.git
-git push -u origin main
