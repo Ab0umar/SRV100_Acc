65f0795d4e22c9cc7f456b5503e2cf19504fb63e
Ab0umar <dr.elsa3dany@gmail.com>
2026-05-20 19:19:31 +0300

    new attn sys

--- client/src/lib/trpc.ts
+++ client/src/lib/trpc.ts
@@ -1,4 +1,6 @@
 import { createTRPCReact } from "@trpc/react-query";
+import superjson from "superjson";
 import type { AppRouter } from "../../../server/routers";
 
 export const trpc = createTRPCReact<AppRouter>();
+export const transformer = superjson;
